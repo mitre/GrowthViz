c1d59e72fafdd8a4bc6792c3ad61b0b935dfa8e2
Meredith Anderson <mkanderson@mitre.org>
2021-04-30 17:36:07 -0400

    Update data processing tests, refs #35

--- tests/test_processdata.py
+++ tests/test_processdata.py
@@ -5,10 +5,10 @@ import processdata
 
 class DataTestCase(unittest.TestCase):
     def setUp(self):
-        self.df = pd.read_csv("growthviz-data/sample-adults-cleaned.csv")
+        self.df = pd.read_csv("growthviz-data/sample-adults-data.csv")
 
     def test_setup_individual_obs(self):
-        setup_df = processdata.setup_individual_obs_df(self.df, "adults")
+        setup_df = processdata.setup_individual_obs_df(self.df)
         self.assertEqual(
             set(
                 [
@@ -19,7 +19,6 @@ class DataTestCase(unittest.TestCase):
                     "age",
                     "sex",
                     "clean_value",
-                    "reason",
                     "clean_cat",
                     "include",
                 ]
@@ -28,23 +27,23 @@ class DataTestCase(unittest.TestCase):
         )
 
     def test_keep_age_range(self):
-        setup_df = processdata.setup_individual_obs_df(self.df, "adults")
+        setup_df = processdata.setup_individual_obs_df(self.df)
         keep_df = processdata.keep_age_range(setup_df, "adults")
         self.assertTrue(len(setup_df) >= len(keep_df))
 
-    def setup_keep_merge(self, df, mode):
-        setup_df = processdata.setup_individual_obs_df(df, mode)
-        keep_df = processdata.keep_age_range(setup_df, mode)
-        merge_df = processdata.setup_merged_df(keep_df, mode)
+    def setup_keep_merge(self, df):
+        setup_df = processdata.setup_individual_obs_df(df)
+        keep_df = processdata.keep_age_range(setup_df, "adults")
+        merge_df = processdata.setup_merged_df(keep_df)
         return merge_df
 
     def test_no_nans(self):
-        merge_df = self.setup_keep_merge(self.df, "adults")
+        merge_df = self.setup_keep_merge(self.df)
         self.assertEqual(0, merge_df["age"].isnull().sum())
         self.assertEqual(0, merge_df["sex"].isnull().sum())
 
     def test_cols(self):
-        merge_df = self.setup_keep_merge(self.df, "adults")
+        merge_df = self.setup_keep_merge(self.df)
         self.assertEqual(
             set(
                 [
@@ -55,7 +54,6 @@ class DataTestCase(unittest.TestCase):
                     "height_cat",
                     "include_height",
                     "height",
-                    "reason",
                     "weight_cat",
                     "include_weight",
                     "weight",
@@ -68,7 +66,7 @@ class DataTestCase(unittest.TestCase):
         )
 
     def test_sex(self):
-        merge_df = self.setup_keep_merge(self.df, "adults")
+        merge_df = self.setup_keep_merge(self.df)
         self.assertTrue(merge_df["sex"].isin([0, 1]).all())
 
 
