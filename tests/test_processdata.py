18b0f29b7688ec39173612adbc1bdc2688ffc731
Daniel Chudnov <dlchudnov@mitre.org>
2021-05-19 12:04:38 -0400

    Modularized DataTestCase, added pediatrics, closes #26

--- tests/test_processdata.py
+++ tests/test_processdata.py
@@ -5,9 +5,13 @@ import pandas as pd
 from growthviz import processdata
 
 
-class DataTestCase(unittest.TestCase):
+class DataTestCase:
+
+    SAMPLE_DATA = "path/to/sample/data.csv"
+    MODE = "adults or pediatrics"
+
     def setUp(self):
-        self.df = pd.read_csv("growthviz-data/sample-adults-data.csv")
+        self.df = pd.read_csv(self.SAMPLE_DATA)
 
     def test_setup_individual_obs(self):
         setup_df = processdata.setup_individual_obs_df(self.df)
@@ -30,12 +34,12 @@ class DataTestCase(unittest.TestCase):
 
     def test_keep_age_range(self):
         setup_df = processdata.setup_individual_obs_df(self.df)
-        keep_df = processdata.keep_age_range(setup_df, "adults")
+        keep_df = processdata.keep_age_range(setup_df, self.MODE)
         self.assertTrue(len(setup_df) >= len(keep_df))
 
     def setup_keep_merge(self, df):
         setup_df = processdata.setup_individual_obs_df(df)
-        keep_df = processdata.keep_age_range(setup_df, "adults")
+        keep_df = processdata.keep_age_range(setup_df, self.MODE)
         merge_df = processdata.setup_merged_df(keep_df)
         return merge_df
 
@@ -70,6 +74,18 @@ class DataTestCase(unittest.TestCase):
     def test_sex(self):
         merge_df = self.setup_keep_merge(self.df)
         self.assertTrue(merge_df["sex"].isin([0, 1]).all())
+
+
+class AdultDataTestCase(DataTestCase, unittest.TestCase):
+
+    SAMPLE_DATA = "growthviz-data/sample-adults-data.csv"
+    MODE = "adults"
+
+
+class PediatricDataTestCase(DataTestCase, unittest.TestCase):
+
+    SAMPLE_DATA = "growthviz-data/sample-pediatrics-data.csv"
+    MODE = "pediatrics"
 
 
 class PctAdultTestCase(unittest.TestCase):
